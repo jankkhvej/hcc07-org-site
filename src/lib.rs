14260086b16beebffdb666ff05328ad3bbb11a60
Alex <solyanik.alexander@gmail.com>
2025-12-18 00:16:08 +0100

    Separated client and server code into different rust packages. Changed styles.

--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn test() -> String{
-    String::from("Hi :p")
-}
--- static/index.js
+++
@@ -1,9 +0,0 @@
-import init, { test } from './pkg/hcc07_org_site.js'
-
-async function run() {
-  await init("./pkg/hcc07_org_site_bg.wasm");
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerText = test();
-}
-
-run();
